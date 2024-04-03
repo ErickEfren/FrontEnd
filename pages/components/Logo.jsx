7cf67b042ec2aa2aa908e238a234f752b388dd37
ErickEfren <erosalesb2000@alumno.ipn.mx>
2024-04-03 12:36:09 -0600

    nueva act

--- pages/components/Logo.jsx
+++ pages/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
       <img
         loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/242b954d62946af00db78059b31f4ee58b83f77a84f1958f976d5d4efea14d78?apiKey=b543ac7ff67748148da449abd9cad6af&"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/6be1696664dc31822bdcde7622029683efd28ce32fa3393ac000d84a1321352d?apiKey=b543ac7ff67748148da449abd9cad6af&"
         alt="Hero Image"
         className="self-stretch w-full aspect-[2.5] fill-[linear-gradient(180deg,#EBB51C_0%,#FAFF00_100%)] max-md:max-w-full"
       />
